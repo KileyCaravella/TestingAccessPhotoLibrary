01ceebb9f3232bbe9a3fc3d020ea39b816058d8d
kiley1323 <kileycaravella@kileys-macbook-pro.local>
2016-06-21 09:42:26 -0400

    Initial Commit

---
+++ TestingAccessPhotoLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingAccessPhotoLibrary
+//
+//  Created by Kiley  Caravella on 6/21/16.
+//  Copyright © 2016 Kiley Caravella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
